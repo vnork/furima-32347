a6aba629077a60e4a95ee385fab32739467a4a86
vnork <esriku-990124@softbank.ne.jp>
2021-04-11 13:27:11 +0900

    ユーザー管理機能実装まで

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email,                 uniqueness: true
+  validates :password,              length: {minimum:6}, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])\w{7,}+\z/, message: 'Include both letters and numbers'}
+  validates :nick_name,             presence: true
+  validates :last_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters'}
+  validates :first_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters'}
+  validates :last_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'}
+  validates :first_name_kana,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'}
+  validates :birth_date,            presence: true
+end
