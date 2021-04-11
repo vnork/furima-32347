1996fe2fc577109098875b24173ee231b2845865
vnork <esriku-990124@softbank.ne.jp>
2021-04-11 20:02:40 +0900

    Rubocop実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :nick_name, :first_name, :first_name_kana, :last_name, :last_name_kana, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:email, :encrypted_password, :nick_name, :first_name, :first_name_kana, :last_name,
+                                             :last_name_kana, :birth_date])
   end
 end
