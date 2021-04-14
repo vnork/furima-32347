0124b2d323c3382d73a11deb5dfb5381c6f6245c
vnork <esriku-990124@softbank.ne.jp>
2021-04-14 21:41:54 +0900

    ActiveHashものmodel作成及びアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :shipping_schedule
   belongs_to :user
 end
