722b0cac0358d38447d85afc22d7000f8a72ae73
vnork <esriku-990124@softbank.ne.jp>
2021-04-26 22:40:29 +0900

    商品削除機能修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -23,11 +23,20 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id == @item.user.id && request.referer&.include?(item_path(@item.id))
-    else
+    unless current_user.id == @item.user.id && request.referer&.include?(item_path(@item.id)) then
       redirect_to root_path
     end
+    # ifの条件文の後に処理を記述した場合
+    #if request.referer&.include?(item_path(@item.id)) 
+    #  if current_user.id != @item.user.id 
+    #    redirect_to root_path
+    #  end
+    #else
+    #  redirect_to root_path
+    #end
   end
+
+
 
   def update
     if @item.update(item_params)
