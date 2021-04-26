7eaceb68f5b9f5bf479d5d1df177f93b976ddbc9
vnork <esriku-990124@softbank.ne.jp>
2021-04-26 15:14:35 +0900

    Rubocop実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe Item, type: :model do
       expect(@item).to be_valid
     end
     it 'priceが9,999,999円なら出品できる' do
-    # 1円単位を使用不可にする場合は下記を使用 
-    # it 'priceが9,999,990円なら出品できる' do
+      # 1円単位を使用不可にする場合は下記を使用
+      # it 'priceが9,999,990円なら出品できる' do
       # @item.sell_price = 9999990
       @item.sell_price = 9_999_999
       expect(@item).to be_valid
@@ -73,7 +73,7 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include('Sell price が範囲外です')
     end
     it 'sell_priceが9,999,999円以上だと出品できない' do
-      # 1円単位を使用不可にする場合は下記を使用 
+      # 1円単位を使用不可にする場合は下記を使用
       # it 'sell_priceが9,999,990円以上だと出品できない' do
       @item = build(:item, sell_price: 10_000_000)
       @item.valid?
@@ -96,9 +96,9 @@ RSpec.describe Item, type: :model do
     end
     # 1円単位を使用不可にする場合は下記を使用
     # it 'sell_priceの1の位が0以外では登録できないこと' do
-      # @item.sell_price = "301"
-      # @item.valid?
-      # expect(@item.errors.full_messages).to include('Sell price は10円単位で設定してください')
+    # @item.sell_price = "301"
+    # @item.valid?
+    # expect(@item.errors.full_messages).to include('Sell price は10円単位で設定してください')
     # end
   end
 end
