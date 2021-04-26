af01da5bc55a906c2aba0313af3837c61667696d
vnork <esriku-990124@softbank.ne.jp>
2021-04-26 17:32:50 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -46,5 +45,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :item_name, :info, :category_id, :sales_status_id, :sell_price,
                                  :shipping_fee_status_id, :shipping_schedule_id, :prefecture_id).merge(user_id: current_user.id)
   end
-
 end
