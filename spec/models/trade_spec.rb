359cded1e9d9bf3ded328690464694acaff64092
vnork <esriku-990124@softbank.ne.jp>
2021-04-29 21:51:25 +0900

    sleepメソッド追加

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -4,7 +4,9 @@ require 'faker'
 RSpec.describe Form, type: :model do
   before do
     @user = build(:user)
+    sleep(0.1)
     @item = build(:item)
+    sleep(0.1)
     @form = build(:form, user_id: @user.id, item_id: @item.id)
   end
 
